4eeb0551d96634128152db9aa0af56c9d07164a9
rajkonkret <rajkonkret660@gmail.com>
2019-11-26 23:57:14 +0100

    dodano wyświetlanie po warunku

--- src/Run.java
+++ src/Run.java
@@ -1,8 +1,8 @@
 public class Run {
     //    Stwórz klasę Opona.
-//    a.Stwórz w niej 3 pola:width, aspectRatio, dimeter oraz konstruktor 3 parametrowy.
+//    a.Stwórz w niej 3 pola:width, aspectRatio, diameter oraz konstruktor 3 parametrowy.
 //    b.Stwórz metodę która zwraca String skrót opony:[175/70 R15]
-//    legenda:["width"/"aspectRatio"R”dimeter"]
+//    legenda:["width"/"aspectRatio"R”diameter"]
 //    c.Stwórz przykładowe obiekty
     public static void main(String[] args) {
         TireStorage storage = new TireStorage();
@@ -12,11 +12,14 @@ public class Run {
         System.out.println(tire.getTire());
 
         storage.setaddTire(tire, 1, 1, 1);
-        storage.setaddTire(tire2, 1, 1, 1);
+        storage.setaddTire(tire2, 1, 3, 1);
         storage.setaddTire(tire, 1, 2, 1);
         storage.setaddTire(tire, 1, 4, 1);
-
-        tire = storage.getTire(1, 1, 1);
-        System.out.println(tire.getTire());
+        for (int i = 0; i < 9; i++) {
+            tire = storage.getTire(1, i, 1);
+            if (tire != null && tire.getTire().equalsIgnoreCase("[185/70 R15]")) {
+                System.out.println("1 " + i + " 1 " + tire.getTire());
+            }
+        }
     }
 }
